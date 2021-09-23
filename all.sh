8ab1116f7f033df5febfdd5fb243369d598e236c
Dmytro <dmytro.kashchuk@studenti.unitn.it>
2021-09-23 13:21:41 +0200

    some changes in eth

--- all.sh
+++ all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "STARTED SETUP SCRIPT"
+
 file="NetStruct.txt"
 
 if [ -f "$file" ] ; then
@@ -26,27 +28,27 @@ SW_route2SW_work=$( (head -5 NetStruct.txt | tail -n -1 )| cut -d' ' -f2)
 
 SW_work2SW_route=$( (head -5 NetStruct.txt | tail -n -1 )| cut -d' ' -f9)
 
-echo "Interface of NWrouter to NwWorkStation1  $NW_route2NW_work"
+echo "Interface of NWrouter to NwWorkStation1:  $NW_route2NW_work"
 
-echo "Interface of NwWorkStation1 to NWrouter $NW_work2NW_route"
+echo "Interface of NwWorkStation1 to NWrouter: $NW_work2NW_route"
 
-echo "Interface NWrouter to IS router $NW_route2IS_route"
+echo "Interface NWrouter to IS router: $NW_route2IS_route"
 
-echo "ISrouter to NW router $IS_route2NW_route"
+echo "ISrouter to NW router: $IS_route2NW_route"
 
-echo "ISrouter to SWrouter $IS_route2SW_route"
+echo "ISrouter to SWrouter: $IS_route2SW_route"
 
-echo "SWrouter to IS router $SW_route2IS_route"
+echo "SWrouter to IS router: $SW_route2IS_route"
 
-echo "SWrouter to SWwrokstation1 $SW_route2SW_work"
+echo "SWrouter to SWwrokstation1: $SW_route2SW_work"
 
-echo "SWwrokstatuib1 to SWrouter $SW_work2SW_route"
+echo "SWwrokstatuib1 to SWrouter: $SW_work2SW_route"
 
 ssh NWworkstation1.Kashchuk-IWorking.offtech "sudo sh ~/offtech/NWworkingstatio1.sh $NW_work2NW_route"
 
 ssh NWrouter.Kashchuk-IWorking.offtech "sudo sh ~/offtech/NWrouter.sh $NW_route2NW_work $NW_route2IS_route"
 
-ssh IWrouter.Kashchuk-IWorking.offtech "sudo sh ~/offtech/ISrouter.sh $IS_route2NW_route $IS_route2SW_route"
+ssh ISrouter.Kashchuk-IWorking.offtech "sudo sh ~/offtech/ISrouter.sh $IS_route2NW_route $IS_route2SW_route"
 
 ssh SWrouter.Kashchuk-IWorking.offtech "sudo sh ~/offtech/SWrouter.sh $SW_route2IS_route $SW_route2SW_work"
 
